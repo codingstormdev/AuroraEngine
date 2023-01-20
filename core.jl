0d09d2d9c7e5a836d0919f28b3bcbb4326eb4fe9
Nick Yfimsky <nikita.nikiforov.2015@mail.ru>
2023-01-20 22:07:53 +0300

    build 1
    nothing interesting here. work just started

---
+++ core.jl
@@ -0,0 +1,15 @@
+using Gtk
+
+mainwindow = GtkWindow("AuroraEngine Executable", 400, 200)
+
+push!(mainwindow) # here you can add your own Gtk things
+
+
+
+"""
+heres should be placed main code
+"""
+
+
+
+showall(mainwindow)
